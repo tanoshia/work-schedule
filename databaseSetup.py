73cfca19994a257e3d42f6d4c7b26c2830a387df
Ethan Shaw <ethan.git@shawpad.com>
2024-03-05 01:52:25 -0800

    moved sample data to seperate file, added messages table

--- databaseSetup.py
+++ databaseSetup.py
@@ -2,24 +2,25 @@ import sqlite3
 
 #define connection and cursor
 connection = sqlite3.connect('employeeShifts.db')
-
 cursor = connection.cursor()
+
+cursor.execute("PRAGMA foreign_keys = ON;")
 
 #TEMP reset tables from prior initalizations, comment out to edit db
 cursor.execute("DELETE FROM employees")
 cursor.execute("DELETE FROM empShifts")
 
 #create employees table
-command1 = """CREATE TABLE IF NOT EXISTS employees (
-empID INTEGER AUTO_INCREMENT PRIMARY KEY,
+createEmployeesTable = """CREATE TABLE IF NOT EXISTS employees (
+empID INTEGER PRIMARY KEY,
 name TEXT,
 position TEXT,
-sickHours)"""
+sickHours INTEGER)"""
 
-cursor.execute(command1)
+cursor.execute(createEmployeesTable)
 
 #create shift table
-command2 = """CREATE TABLE IF NOT EXISTS empShifts (
+createEmpShiftsTable = """CREATE TABLE IF NOT EXISTS empShifts (
 shiftID INTEGER PRIMARY KEY,
 empID INTEGER,
 shiftDate DATE,
@@ -27,64 +28,17 @@ startTime TIME,
 endTime TIME,
 FOREIGN KEY(empID) REFERENCES employees(empID))"""
 
-cursor.execute(command2)
+cursor.execute(createEmpShiftsTable)
 
-#add to employees SAMPLE DATA
-cursor.execute("""INSERT INTO employees 
-               (name, position) VALUES 
-               ('Brett Vandenburg', 'Manager')""")
-cursor.execute("""INSERT INTO employees 
-               (name, position) VALUES 
-               ('David Thoe', 'Manager')""")
-cursor.execute("""INSERT INTO employees 
-               (name, position) VALUES 
-               ('Ethan Shaw', 'Worker')""")
-cursor.execute("""INSERT INTO employees 
-               (name, position) VALUES 
-               ('Adam Marks', 'Worker')""")
-connection.commit()
+#create messages table
+createEmpShiftsTable = """CREATE TABLE IF NOT EXISTS messages (
+messageID       INTEGER         NOT NULL        PRIMARY KEY,
+empID           INTEGER         NOT NULL,
+mngrID          INTEGER         NOT NULL,
+messageBody     TEXT            NOT NULL,
+FOREIGN KEY(empID) REFERENCES employees(empID),
+FOREIGN KEY(mngrID) REFERENCES employees(empID))"""
 
-#add to empShifts SAMPLE DATA
-# cursor.execute("""INSERT INTO empShifts (empID, shiftDate, startTime, endTime) VALUES (23, '2024-01-24', '08:00', '16:00')""")
-
-cursor.execute("""INSERT INTO empShifts 
-(empID, shiftDate, startTime, endTime) VALUES 
-(1, '2024-03-04', '07:00', '15:00'),
-(1, '2024-03-05', '07:00', '15:00'),
-(1, '2024-03-06', '07:00', '15:00'),
-(1, '2024-03-07', '06:00', '14:00'),
-(1, '2024-03-08', '07:00', '15:00')""")
-
-cursor.execute("""INSERT INTO empShifts 
-(empID, shiftDate, startTime, endTime) VALUES 
-(2, '2024-03-06', '08:00', '16:00'),
-(2, '2024-03-07', '07:00', '15:00'),
-(2, '2024-03-08', '08:00', '16:00'),
-(2, '2024-03-09', '07:00', '15:00'),
-(2, '2024-03-10', '07:00', '15:00')""")
-
-cursor.execute("""INSERT INTO empShifts 
-(empID, shiftDate, startTime, endTime) VALUES 
-(3, '2024-03-04', '17:00', '21:30'),
-(3, '2024-03-05', '14:00', '21:30'),
-(3, '2024-03-07', '08:00', '16:00'),
-(3, '2024-03-09', '07:00', '15:00'),
-(3, '2024-03-10', '07:00', '15:00')""")
-
-cursor.execute("""INSERT INTO empShifts 
-(empID, shiftDate, startTime, endTime) VALUES 
-(4, '2024-03-04', '11:00', '18:00'),
-(4, '2024-03-05', '11:00', '18:00'),
-(4, '2024-03-08', '11:00', '18:00'),
-(4, '2024-03-09', '11:00', '18:00'),
-(4, '2024-03-10', '11:00', '18:00')""")
-
-connection.commit()
-
-
-#get results
-cursor.execute("SELECT * FROM empShifts")
-results = cursor.fetchall()
-print(results)
+cursor.execute(createEmpShiftsTable)
 
 connection.close()
