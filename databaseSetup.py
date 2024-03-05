505b6d16ccfae1319b4369db2b98dccd06d18270
Ethan Shaw <ethan.git@shawpad.com>
2024-03-05 04:24:48 -0800

    added messages table

--- databaseSetup.py
+++ databaseSetup.py
@@ -4,17 +4,19 @@ import sqlite3
 connection = sqlite3.connect('employeeShifts.db')
 cursor = connection.cursor()
 
-cursor.execute("PRAGMA foreign_keys = ON;")
+# reset db
+cursor.execute("DROP TABLE IF EXISTS employees")
+cursor.execute("DROP TABLE IF EXISTS empShifts")
+cursor.execute("DROP TABLE IF EXISTS messages")
+connection.commit()
 
-#TEMP reset tables from prior initalizations, comment out to edit db
-cursor.execute("DELETE FROM employees")
-cursor.execute("DELETE FROM empShifts")
+cursor.execute("PRAGMA foreign_keys = ON;")
 
 #create employees table
 createEmployeesTable = """CREATE TABLE IF NOT EXISTS employees (
 empID INTEGER PRIMARY KEY,
-name TEXT,
-position TEXT,
+name VARCHAR(64),
+position VARCHAR(64),
 sickHours INTEGER)"""
 
 cursor.execute(createEmployeesTable)
@@ -31,14 +33,15 @@ FOREIGN KEY(empID) REFERENCES employees(empID))"""
 cursor.execute(createEmpShiftsTable)
 
 #create messages table
-createEmpShiftsTable = """CREATE TABLE IF NOT EXISTS messages (
+createMessagesTable = """CREATE TABLE IF NOT EXISTS messages (
 messageID       INTEGER         NOT NULL        PRIMARY KEY,
 empID           INTEGER         NOT NULL,
 mngrID          INTEGER         NOT NULL,
-messageBody     TEXT            NOT NULL,
+timestamp       VARCHAR(255)    NOT NULL,
+messageBody     VARCHAR(255)    NOT NULL,
 FOREIGN KEY(empID) REFERENCES employees(empID),
 FOREIGN KEY(mngrID) REFERENCES employees(empID))"""
 
-cursor.execute(createEmpShiftsTable)
+cursor.execute(createMessagesTable)
 
 connection.close()
